413a5a52fa101efc6be5481af1fad842de40b5aa
Michael Calcinai <michael@calcin.ai>
2016-06-14 15:48:09 +0200

    Comment

--- lib/registration.php
+++ lib/registration.php
@@ -9,6 +9,7 @@ stream_register_wrapper('mmap', \Calcinai\MMap\StreamWrapper::class);
 
 function mmap_open($file_name, $block_size, $offset = 0){
     //TODO - finish these
+    //Happy to suppress error here as an Exception will be thrown on failure.
     $stream = @fopen(sprintf('mmap://%s:%s?offset=%s', $file_name, $block_size, $offset), 'rw');
     return $stream;
 }
