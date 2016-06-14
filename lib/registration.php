187948be1bb70313f3787dd2852d61d395c8096a
Michael Calcinai <michael@calcin.ai>
2016-06-14 15:47:36 +0200

    Suppress error on fopen as an exception will throw

--- lib/registration.php
+++ lib/registration.php
@@ -9,6 +9,6 @@ stream_register_wrapper('mmap', \Calcinai\MMap\StreamWrapper::class);
 
 function mmap_open($file_name, $block_size, $offset = 0){
     //TODO - finish these
-    $stream = fopen(sprintf('mmap://%s:%s?offset=%s', $file_name, $block_size, $offset), 'rw');
+    $stream = @fopen(sprintf('mmap://%s:%s?offset=%s', $file_name, $block_size, $offset), 'rw');
     return $stream;
 }
