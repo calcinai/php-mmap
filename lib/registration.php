1b77cabb554c1ae49787e60307f3dcdf94f12448
Michael Calcinai <michael@calcin.ai>
2016-07-01 21:44:31 +0200

    Accommodates php extension

--- lib/registration.php
+++ lib/registration.php
@@ -4,13 +4,16 @@
  * @author     Michael Calcinai <michael@calcin.ai>
  */
 
-//Kindof a backward way go get it as a resource.
-stream_register_wrapper('mmap', '\\Calcinai\\MMap\\StreamWrapper');
-//stream_register_wrapper('mmap', \Calcinai\MMap\StreamWrapper::class); //Only thing here that requires php5.5
 
-function mmap_open($file_name, $block_size, $offset = 0){
-    //TODO - finish these
-    //Happy to suppress error here as an Exception will be thrown on failure.
-    $stream = @fopen(sprintf('mmap://%s:%s?offset=%s', $file_name, $block_size, $offset), 'rw');
-    return $stream;
+if(!function_exists('mmap_open')){
+    //stream_register_wrapper('mmap', \Calcinai\MMap\StreamWrapper::class); //Only thing here that requires php5.5
+    stream_register_wrapper('mmap', '\\Calcinai\\MMap\\StreamWrapper');
+
+    //Kindof a backward way go get it as a resource.
+    function mmap_open($file_name, $block_size, $offset = 0){
+        //TODO - finish these
+        //Happy to suppress error here as an Exception will be thrown on failure.
+        $stream = @fopen(sprintf('mmap://%s:%s?offset=%s', $file_name, $block_size, $offset), 'rw');
+        return $stream;
+    }
 }
